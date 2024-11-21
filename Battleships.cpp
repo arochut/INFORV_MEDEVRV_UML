6756676147aa5595881556bcd75e0a891c0f251f
ARochut <anatole.rochut@eleves.ec-nantes.fr>
2024-11-21 08:52:25 +0100

    Correction

--- Battleships.cpp
+++ Battleships.cpp
@@ -4,9 +4,6 @@
 #include "Battleships.h"
 
 int main(int argc, const char* argv[]) {
-	//manipulation_image();
-	//filtre_convolution();
-	// transformee_fourier();
-	morphologie();
+	
 	return 0;
 }
