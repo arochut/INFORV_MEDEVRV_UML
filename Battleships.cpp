272c79b4dcbf31c0c324e8e8051324e8e5f293aa
ARochut <anatole.rochut@eleves.ec-nantes.fr>
2024-11-21 08:51:42 +0100

    Initial commit

---
+++ Battleships.cpp
@@ -0,0 +1,12 @@
+// TestOpenCV.cpp : Defines the entry point for the application.
+//
+
+#include "Battleships.h"
+
+int main(int argc, const char* argv[]) {
+	//manipulation_image();
+	//filtre_convolution();
+	// transformee_fourier();
+	morphologie();
+	return 0;
+}
