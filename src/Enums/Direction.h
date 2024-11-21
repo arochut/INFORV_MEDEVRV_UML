18768a5460238e1d98d578bd9f36f2ebcb04323f
ARochut <anatole.rochut@eleves.ec-nantes.fr>
2024-11-21 09:42:47 +0100

    WIp

---
+++ src/Enums/Direction.h
@@ -0,0 +1,12 @@
+// Create a enum for direction : Vertical/Horizontal
+
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+enum class Direction
+{
+    Vertical,
+    Horizontal
+};
+
+#endif // DIRECTION_H
---
+++ src/Ship.h
@@ -0,0 +1,21 @@
+
+
+#include "Direction.h"
+#include <vector>
+#include <string>
+#include <pair>
+
+class Ship {
+
+private:
+    int x_start;
+    int y_start;
+    int size;
+    Direction dir;
+
+    string name;
+
+
+
+
+}
