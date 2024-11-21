ffd63d1fe1e6b1bd61f6c4fa080369b39103a101
ARochut <anatole.rochut@eleves.ec-nantes.fr>
2024-11-21 09:48:41 +0100

    Ship first iteration

---
+++ src/Ship.cpp
@@ -0,0 +1,43 @@
+Ship::Ship(int x, int y, int size, Direction dir, string name) {
+    x_start = x;
+    y_start = y;
+    this->size = size;
+    this->dir = dir;
+    this->name = name;
+    hits = vector<bool>(size, false);
+}
+
+bool Ship::is_dead() {
+    for (int i = 0; i < size; i++) {
+        if (!hits[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool Ship::occupies(int x, int y) {
+    if (dir == Direction::Horizontal) {
+        if (x < x_start || x >= x_start + size) {
+            return false;
+        }
+        return y == y_start;
+    } else {
+        if (y < y_start || y >= y_start + size) {
+            return false;
+        }
+        return x == x_start;
+    }
+}
+
+bool Ship::hit(int x, int y) {
+    if (!occupies(x, y)) {
+        return false;
+    }
+    if (dir == Direction::Horizontal) {
+        hits[x - x_start] = true;
+    } else {
+        hits[y - y_start] = true;
+    }
+    return true;
+}
--- src/Ship.h
+++ src/Ship.h
@@ -14,8 +14,14 @@ private:
     Direction dir;
 
     string name;
+    vector<bool> hits;
+    // int hit_score;
 
-
-
-
+public:
+    Ship(int x, int y, int size, Direction dir, string name);
+    bool is_dead();
+    bool occupies(int x, int y);
+    bool hit(int x, int y);
+    
+    
 }
