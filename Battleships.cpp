9e1d7f14ccc35585dc21e48f20f2b2602dec0593
Gauthier <yo>
2024-11-21 11:24:22 +0100

    Update Battleships.cpp

--- Battleships.cpp
+++ Battleships.cpp
@@ -1,9 +1,9 @@
-// TestOpenCV.cpp : Defines the entry point for the application.
-//
 
 #include "Battleships.h"
+#include "src/Board.h"
 
 int main(int argc, const char* argv[]) {
-
+	Board boardA = Board();
+	boardA.display();
 	return 0;
 }
