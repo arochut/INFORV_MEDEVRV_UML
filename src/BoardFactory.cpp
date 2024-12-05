9c0cb4543e4f3968bcaa9678d103a1faabd07766
ARochut <anatole.rochut@eleves.ec-nantes.fr>
2024-12-05 10:45:42 +0100

    Fix direction

--- src/BoardFactory.cpp
+++ src/BoardFactory.cpp
@@ -13,13 +13,13 @@ void BoardFactory::place_ship(Board& board, ShipType type) {
     pair<int, int> xy = make_pair(-1, -1);
     Direction dir;
     while(!input_correct)  {
-    cout << "Ou voulez-vous placer votre " << type.name << " (taille " << type.size << ") ?" << endl;
-    board.display_own();
-    cout << "Entrez les coordonnées sous la forme 'B4' :" << endl;
+        cout << "Ou voulez-vous placer votre " << type.name << " (taille " << type.size << ") ?" << endl;
+        board.display_own();
+        cout << "Entrez les coordonnées sous la forme 'B4' :" << endl;
         string coord;
         cin >> coord;
         char d;
-    cout << "Entrez la direction du bateau (h pour horizontal, v pour vertical) :" << endl;
+        cout << "Entrez la direction du bateau (h pour horizontal, v pour vertical) :" << endl;
         cin >> d;
         xy = Board::get_coords(coord);
         if (xy.first != -1 && xy.second != -1) {
@@ -35,6 +35,7 @@ void BoardFactory::place_ship(Board& board, ShipType type) {
         else {
             cout << "Direction invalide" << endl;
             xy = make_pair(-1, -1);
+            input_correct = false;
         }
         
 
