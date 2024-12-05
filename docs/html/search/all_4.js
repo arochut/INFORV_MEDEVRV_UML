a0465cc012d7e2e6cb8d11a5641218cfffcbcc32
ARochut <anatole.rochut@eleves.ec-nantes.fr>
2024-12-05 08:35:34 +0100

    Doc with included files

---
+++ docs/html/search/all_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['ship_0',['Ship',['../d3/d1d/class_ship.html',1,'']]],
+  ['shiptype_1',['ShipType',['../d6/d9c/struct_ship_type.html',1,'']]],
+  ['shiptypes_2',['ShipTypes',['../d0/d58/class_ship_types.html',1,'']]]
+];
---
+++ docs/html/search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['gamemanager_0',['GameManager',['../db/d10/class_game_manager.html',1,'']]]
+];
